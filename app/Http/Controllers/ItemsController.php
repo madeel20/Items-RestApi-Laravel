2f9a3f4f64e4adbd51ed69da06b9d204285c6adc
Muhammad Adeel <smaa.link@gmail.com>
2019-09-07 10:36:33 +0500

    final

---
+++ app/Http/Controllers/ItemsController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Item;
+use Validator;
+
+class ItemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Item::all();
+        return response()->json($items);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+          'text' => 'required'
+        ]);
+
+        if($validator->fails()){
+          $response = array('response' => $validator->messages(), 'success' => false);
+          return $response;
+        } else {
+          // Create item
+          $item = new Item;
+          $item->text = $request->input('text');
+          $item->body = $request->input('body');
+          $item->save();
+
+          return response()->json($item);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $item = Item::find($id);
+        return response()->json($item);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+      $validator = Validator::make($request->all(),[
+        'text' => 'required'
+      ]);
+
+      if($validator->fails()){
+        $response = array('response' => $validator->messages(), 'success' => false);
+        return $response;
+      } else {
+        // Find an item
+        $item = Item::find($id);
+        $item->text = $request->input('text');
+        $item->body = $request->input('body');
+        $item->save();
+
+        return response()->json($item);
+      }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      // Find an item
+      $item = Item::find($id);
+      $item->delete();
+
+      $response = array('response' => 'Item deleted', 'success' => true);
+      return $response;
+    }
+}
